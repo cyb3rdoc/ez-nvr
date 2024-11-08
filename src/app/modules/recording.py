30276d700b7b7653f5def4b2f9131c2f034157b0
cyb3rdoc <cyb3rdoc@proton.me>
2024-11-08 19:20:31 +0400

    Failed streams clean up

--- src/app/modules/recording.py
+++ src/app/modules/recording.py
@@ -6,6 +6,9 @@ from utils.logger import log_info, log_error
 from utils.common import stop_flags, stop_flags_lock
 
 
+# Dictionary to hold ffmpeg process for each camera
+camera_processes = {}
+
 def start_recording(cam_config, stop_flag):
     cam_name = cam_config['camera_name']
     cam_ip = cam_config['camera_ip']
@@ -41,8 +44,14 @@ def start_recording(cam_config, stop_flag):
     cmd = f"ffmpeg -hide_banner -y -loglevel error -rtsp_transport tcp -use_wallclock_as_timestamps 1 -i \"{rtsp_url}\" -c {codec} -f segment -reset_timestamps 1 -segment_time {interval} -segment_format mkv -segment_atclocktime 1 -strftime 1 {raw_path}/%Y-%m-%dT%H-%M-%S.mkv"
     while not stop_flag.is_set():
         try:
+            log_info(f"NVR: Starting recording for {cam_name} at {rtsp_url}")
             process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             log_info(f"NVR: Camera {cam_name} initialized.")
+            # Track the process
+            with stop_flags_lock:
+                camera_processes[cam_name] = process
+            # Wait for the process to exit (non-blocking)
+            process.wait()
             return process
         except subprocess.CalledProcessError as e:
             log_error(f"NVR: Error starting recording: {e.output}")
@@ -56,3 +65,19 @@ def stop_recording(cam_name):
             stop_flags[cam_name].set()
         else:
             log_error(f"NVR: Camera {cam_name} not found in stop_flags.")
+        # Check if the process is still running before terminating it
+        if cam_name in camera_processes:
+            process = camera_processes[cam_name]
+            # Check if the process is still alive
+            if process.poll() is None:
+                log_info(f"NVR: Stopping ffmpeg process for {cam_name}.")
+                process.terminate() # Gracefully terminate ffmpeg
+                process.wait() # Wait for process to terminate
+                del camera_processes[cam_name] # Clean up the process dictionary
+                log_info(f"NVR: Stopped ffmpeg process for {cam_name}.")
+            else:
+                # The ffmpeg process has crashed or already exited
+                log_error(f"NVR: ffmpeg for {cam_name} has already crashed or terminated unexpectedly.")
+                del camera_processes[cam_name]  # Clean up the process dictionary
+        else:
+            log_error(f"NVR: No ffmpeg process found for {cam_name}.")
