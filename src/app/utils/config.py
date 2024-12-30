5692ebb9d7735aa39bc88e230e121ee797f4b6cc
cyb3rdoc <cyb3rdoc@proton.me>
2024-12-30 13:59:39 +0400

    Input validation & sanitization

--- src/app/utils/config.py
+++ src/app/utils/config.py
@@ -2,13 +2,14 @@ import os
 import yaml
 import re
 import unicodedata
+import ipaddress
 from utils.logger import log_error
 
-# read environment variables
+# Read environment variables
 CONFIG_FILE = os.environ.get('CONFIG_FILE', '/config/config.yaml')
 
 
-def sanitize_input(input_value):
+def sanitize_name(input_value):
     if isinstance(input_value, str):
         # Normalize the string to remove accents
         input_value = unicodedata.normalize('NFKD', input_value).encode('ascii', 'ignore').decode('ascii')
@@ -21,11 +22,42 @@ def sanitize_input(input_value):
         return input_value
 
 
+def validate_ip(ip):
+    try:
+        ipaddress.ip_address(ip)
+        return True
+    except ValueError:
+        return False
+
+
+def validate_rtsp(url):
+    return url.startswith('rtsp://')
+
+
+def validate_codec(codec):
+    supported_codecs = ['copy', 'libx264', 'h264', 'libx265']
+    return codec if codec in supported_codecs else 'copy'
+
+
+def validate_interval(interval):
+    return interval if isinstance(interval, int) and interval > 0 else 300
+
+
+def validate_store(days):
+    return days if isinstance(days, int) and days > 0 else 15
+
+
 def sanitize_config(config):
     sanitized_config = config.copy()
-    for camera_config in sanitized_config.get('cameras', []):
-        # Sanitize user inputs in camera_name field
-        camera_config['camera_name'] = sanitize_input(camera_config.get('camera_name', ''))
+    sanitized_config["video_store"] = validate_store(config.get("video_store", 15))
+    for camera in sanitized_config.get('cameras', []):
+        camera['camera_name'] = sanitize_name(camera.get('camera_name', ''))
+        if not validate_ip(camera.get('camera_ip', '')):
+            log_error(f"Invalid IP address for camera: {camera.get('camera_name', '')}")
+        if not validate_rtsp(camera.get('camera_rtsp', '')):
+            log_error(f"Invalid RTSP URL for camera: {camera.get('camera_name', '')}")
+        camera['camera_codec'] = validate_codec(camera.get('camera_codec', 'copy'))
+        camera['camera_interval'] = validate_interval(camera.get('camera_interval', 300))
     return sanitized_config
 
 
