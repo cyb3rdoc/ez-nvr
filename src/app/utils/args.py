dfd85f97955538e5c98659df0d24dacf1aa5649f
cyb3rdoc <cyb3rdoc@proton.me>
2025-01-05 15:54:42 +0400

    Concatenation bug fix

--- src/app/utils/args.py
+++ src/app/utils/args.py
@@ -1,7 +1,19 @@
+import os
 import argparse
 
-
 def get_args():
+    # First check environment variable, default to False
+    debug_env = os.environ.get('DEBUG', '').lower() in ('true', '1', 'yes')
+
+    # Then check command line args (allows override)
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--debug", help="enable debug logging", action="store_true")
+    parser.add_argument("-d", "--debug",
+                       help="enable debug logging",
+                       action="store_true",
+                       default=False)
+
+    # If environment variable is True, override the default
+    if debug_env:
+        parser.set_defaults(debug=True)
+
     return parser.parse_args()
